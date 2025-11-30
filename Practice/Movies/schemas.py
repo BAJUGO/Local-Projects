a0abf4938d312a71cc823abbd4d9d739c6c60f00
Dima <texetbuy@gmail.com>
2025-11-30 10:17:22 +0300

    Added schemas.py, that contains MoviePost, MovieCreate and Movie as well

---
+++ Practice/Movies/schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Field
+
+
+class MoviePost(BaseModel):
+    title: str
+    director: str
+    description: str
+    year_of_issue: int = Field(ge=1900, le=2025)
+
+
+class MovieCreate(MoviePost):
+    pass
+
+
+class Movie(MoviePost):
+    id: int
+
+    class Config:
+        from_attribute = True
