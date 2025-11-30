d8a7f9ec1bb8e223edef45cc60de9655bcc5d734
Dima <texetbuy@gmail.com>
2025-11-30 10:12:38 +0300

    Created __init__.py and model.py, that contains class Movie

---
+++ Practice/Movies/model.py
@@ -0,0 +1,12 @@
+from .db import BaseClass
+from sqlalchemy import Integer, String, Column
+
+
+class Movie(BaseClass):
+    id = Column(Integer, primary_key=True)
+    title = Column(String, index=True)
+    director = Column(String)
+    description = Column(String, index=True)
+    year_of_issue = Column(Integer)
+
+
