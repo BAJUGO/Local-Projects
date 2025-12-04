86649fe4d5cdc219b3b873a7633cbcc6725fe432
Dima <texetbuy@gmail.com>
2025-12-04 22:03:15 +0300

    i do like websockets

--- Practice/WebSocket/main.py
+++ Practice/WebSocket/main.py
@@ -1,123 +1,7 @@
-from typing import Annotated
-
-from fastapi import (
-    Cookie,
-    Depends,
-    FastAPI,
-    Query,
-    WebSocket,
-    WebSocketException,
-    status,
-    WebSocketDisconnect
-)
+from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 
-#! Вообще веб-сокет бы взаимодействовал с Frontend, или же с мобильным нативным кодом, или ещё чем-то, но здесь для примера
-#! будет банальный код
-
-
 app = FastAPI()
-
-'''Старый html (вебсокет, где ты разговариваешь сам с собой, имея cookie и query'''
-# html = """
-# <!DOCTYPE html>
-# <html>
-#     <head>
-#         <title>Chat</title>
-#     </head>
-#     <body>
-#         <h1>WebSocket Chat</h1>
-#         <form action="" onsubmit="sendMessage(event)">
-#             <label>Item ID: <input type="text" id="itemId" autocomplete="off" value="foo"/></label>
-#             <label>Token: <input type="text" id="token" autocomplete="off" value="some-key-token"/></label>
-#             <button onclick="connect(event)">Connect</button>
-#             <hr>
-#             <label>Message: <input type="text" id="messageText" autocomplete="off"/></label>
-#             <button>Send</button>
-#         </form>
-#         <ul id='messages'>
-#         </ul>
-#         <script>
-#         var ws = null;
-#             function connect(event) {
-#                 var itemId = document.getElementById("itemId")
-#                 var token = document.getElementById("token")
-#                 ws = new WebSocket("ws://localhost:8000/items/" + itemId.value + "/ws?token=" + token.value);
-#                 ws.onmessage = function(event) {
-#                     var messages = document.getElementById('messages')
-#                     var message = document.createElement('li')
-#                     var content = document.createTextNode(event.data)
-#                     message.appendChild(content)
-#                     messages.appendChild(message)
-#                 };
-#                 event.preventDefault()
-#             }
-#             function sendMessage(event) {
-#                 var input = document.getElementById("messageText")
-#                 ws.send(input.value)
-#                 input.value = ''
-#                 event.preventDefault()
-#             }
-#         </script>
-#     </body>
-# </html>
-# """
-
-
-'''Старый код здесь'''
-#! Старый код для простого чата с сайтом (с самим сабой)
-# from fastapi import FastAPI, WebSocket
-# from fastapi.responses import HTMLResponse
-#
-# app = FastAPI()
-#
-# html = """
-# <!DOCTYPE html>
-# <html>
-#     <head>
-#         <title>Chat</title>
-#     </head>
-#     <body>
-#         <h1>WebSocket Chat</h1>
-#         <form action="" onsubmit="sendMessage(event)">
-#             <input type="text" id="messageText" autocomplete="off"/>
-#             <button>Send</button>
-#         </form>
-#         <ul id='messages'>
-#         </ul>
-#         <script>
-#             var ws = new WebSocket("ws://localhost:8000/ws");
-#             ws.onmessage = function(event) {
-#                 var messages = document.getElementById('messages')
-#                 var message = document.createElement('li')
-#                 var content = document.createTextNode(event.data)
-#                 message.appendChild(content)
-#                 messages.appendChild(message)
-#             };
-#             function sendMessage(event) {
-#                 var input = document.getElementById("messageText")
-#                 ws.send(input.value)
-#                 input.value = ''
-#                 event.preventDefault()
-#             }
-#         </script>
-#     </body>
-# </html>
-# """
-#
-#
-# @app.get("/")
-# async def get():
-#     return HTMLResponse(html)
-#
-#
-# @app.websocket("/ws")
-# async def websocket_endpoint(websocket: WebSocket):
-#     await websocket.accept()
-#     while True:
-#         data = await websocket.receive_text()
-#         await websocket.send_text(f"Message text was: {data}")
-
 
 html = """
 <!DOCTYPE html>
@@ -127,59 +11,35 @@ html = """
     </head>
     <body>
         <h1>WebSocket Chat</h1>
-        <h2>Your ID: <span id="ws-id"></span></h2>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
+        <form onsubmit="sendMessage(event)">
+            <input type="text" id="messageInput" autocomplete="off">
+            <button type="submit">Send</button>
         </form>
-        <ul id='messages'>
+        
+        <ul id="messagesList">
         </ul>
+        
         <script>
-            var client_id = Date.now()
-            document.querySelector("#ws-id").textContent = client_id;
-            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
+        var ws = new WebSocket("ws://localhost:8000/ws")
+        
+        ws.onmessage = function(event) {
+        var messagesList = document.getElementById("messagesList")
+        var message = document.createElement("li")
+        message.textContent = event.data
+        messagesList.appendChild(message) };
+        
+        function sendMessage(event) {
+        var input = document.getElementById("messageInput")
+        ws.send(input.value)
+        input.value = ''
+        
+        event.preventDefault()
+        };
+        
         </script>
     </body>
 </html>
 """
-
-
-
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: list[WebSocket] = []
-
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
-
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
-    async def send_personal_message(self,message: str, websocket: WebSocket):
-        await websocket.send_text(message)
-
-
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text(message)
-
-
-manager = ConnectionManager()
-
 
 
 @app.get("/")
@@ -187,46 +47,17 @@ async def get():
     return HTMLResponse(html)
 
 
-async def get_cookie_or_token(
-    websocket: WebSocket,
-    session: Annotated[str | None, Cookie()] = None,
-    token: Annotated[str | None, Query()] = None,
-):
-    if session is None and token is None:
-        raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION)
-    return session or token
+list_of_illegal = ["fuck", "KYS", "fucking", "bitch", "shit"]
 
-
-'''Старый websocket (не первый)'''
-#! Версия Вебсокета, когда я общался сам с собой, передавая Cookie и Query
-
-# @app.websocket("/items/{item_id}/ws")
-# async def websocket_endpoint(
-#     *,
-#     websocket: WebSocket,
-#     item_id: str,
-#     q: int | None = None,
-#     cookie_or_token: Annotated[str, Depends(get_cookie_or_token)],
-# ):
-#     await websocket.accept()
-#     while True:
-#         data = await websocket.receive_text()
-#         await websocket.send_text(
-#             f"Session cookie or query token value is: {cookie_or_token}"
-#         )
-#         if q is not None:
-#             await websocket.send_text(f"Query parameter q is: {q}")
-#         await websocket.send_text(f"Message text was: {data}, for item ID: {item_id}")
-
-
-@app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
-    await manager.connect(websocket)
-    try:
-        while True:
-            data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{client_id} says: {data}")
-    except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat")
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()     # Принимаем websocket соединение. Только после этого можно обмениваться сообщениями при помощи receive и send text
+    while True:   # Бесконечный цикл. Пока клиент не закроет подключение, будет эта ерунда выполняться
+        data = await websocket.receive_text()
+        new_words = []
+        for word in data.split():
+            if word in list_of_illegal:
+                word = "*" * len(word)
+            new_words.append(word)
+        new_data = " ".join(new_words)
+        await websocket.send_text(new_data)
